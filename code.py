a199c465f79b145a221bdda60bea4e1af2187a2f
Dare <joelliju10@gmail.com>
2022-07-14 21:35:48 -0400

    rest of the code

--- code.py
+++ code.py
@@ -1,11 +1,28 @@
 from random import randint
 userpoints, gamepoints = [0,0]
-options = {"rock" : 1,"paper" : 2,"scissor" : 3}
-print(options)
+invertedOptions = ["rock","paper","scissor"]
+options = {"rock" : 1,"paper" : 2,"scissor" : 0}#each number indicates which things beats it
 while True:
-    try:
-        userChoice = input("Enter your option?\n")
-        print(options[userChoice])
-        break
-    except KeyError:
-        print("Something went wrong, Try again")
+    while True:
+        try:
+            print(options)
+            userChoice = input("Enter your option?\n")
+            lossingValue = options[userChoice]
+            break
+        except KeyError:
+            print("Something went wrong, Try again")
+    print()
+    gameChoice = randint(0,2)#0 is rock, 1 is paper, 2 is scissor
+    print("The game choose",invertedOptions[gameChoice])
+    if gameChoice == lossingValue:
+        print("The game wins!")
+        gamepoints = gamepoints + 1
+    elif (lossingValue-1)%3 == gameChoice:
+        print("It is a draw")
+    else:
+        print("You win!")
+        userpoints = userpoints + 1
+    print()
+    print("Your points : %d , The game points : %d" %(userpoints,gamepoints))
+    print()
+    
